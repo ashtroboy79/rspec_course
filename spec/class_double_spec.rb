1a75d15d37700e520d2a7259e6256ccb79168ebd
Ahsan <rasul.ahsan@gmail.com>
2021-09-02 11:55:25 +0100

    class doubles interesting

---
+++ spec/class_double_spec.rb
@@ -0,0 +1,26 @@
+class Deck
+  def self.build
+    # Business logic to build a whole bunch of cards
+  end
+end
+
+class CardGame
+  attr_reader :cards
+
+  def start
+    @cards = Deck.build
+  end
+end
+
+RSpec.describe CardGame do
+  it 'can only implement class methods that are defined on a class' do
+    # -- Failure/Error: class_double(Deck, { build: ['Ace', 'Queen'], shuffle: ['Queen', 'Ace']}) --
+    # -- the Deck class does not implement the class method: shuffle --
+    # class_double(Deck, { build: ['Ace', 'Queen'], shuffle: ['Queen', 'Ace']})
+    deck_klass = class_double(Deck, { build: ['Ace', 'Queen']}).as_stubbed_const
+
+    expect(deck_klass).to receive(:build)
+    subject.start
+    expect(subject.cards).to eq(['Ace', 'Queen'])
+  end
+end
